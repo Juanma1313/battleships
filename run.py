85dcc152118b42463bf6e3b3eeeb0abfd1b950fa
Juan Manuel de las Heras Arroyo <j.heras13@gmail.com>
2023-03-26 07:05:09 +0000

    Add battleships_game() with the game logic to run.py

--- run.py
+++ run.py
@@ -8,8 +8,87 @@ This file contains the main entry point for the Battleships game application.
 It holds also all the console-user interaction and the main game logic.
 '''
 from time import sleep as delay     # delay in seconds to allow player to read screens
-
 from battleships_colors import *    # import available color commands
+
+
+def battleships_game(columns=10,rows=10, name="Player"):
+    ''' Main Battleship game function.
+    It prepares the player and the computer boards and alternatively
+    yields the turn to fire to both.
+    The end of the game is decided when one player has sunk all his ships.
+    The winner is the player who still has some ships afloat.
+    '''
+    # Prepare Computer  battle zone
+    computer_battle_zone = battle_zone(columns=columns, rows=rows, name="Computer")
+    for ship_class in COMPUTER_SHIPS:
+        result = RESULT_UNKNOWN
+        while result != RESULT_MISS:
+            result=computer_battle_zone.new_ship(ship_class)
+        #print(f"{__name__}.main({args}), Computer ship {ship_class} placed")
+
+    #display_battle_zone(computer_battle_zone) # Display battle zone
+
+    # Prepare Player battle zone
+    player_battle_zone = battle_zone(columns=columns, rows=rows, name=name)
+    for ship_class in PLAYER_SHIPS:
+        result = RESULT_UNKNOWN
+        while result != RESULT_MISS:
+            display_battle_zone(player_battle_zone) # Display battle zone
+            # Get new ship location  input from player
+            coordinates, possition =get_new_ship_location(ship_class)
+            if not possition:
+                #Attempt to generate automatic location
+                while result != RESULT_MISS:    # ship colision avoidance loop
+                    result=player_battle_zone.new_ship(ship_class)
+            else:
+                if possition==-1 or coordinates[0]==-1 or coordinates[1]==-1:
+                    # The location could not be validated
+                    delay(3) # delay to let the player read the screen
+
+    while True: # Game loop
+        if sum([ship.sunk for ship in player_battle_zone.ships]) == len(player_battle_zone.ships):
+            # All the player's ships are sunk. End of the game
+            winner = computer_battle_zone
+            break    # end the game, The Computer wins
+        while True:  # Player's turn loop
+            display_battle_zone(player_battle_zone) # Display battle zone
+            coordinates=get_coordinates()
+            if coordinates[0]==None:
+                # Automated coordinate generation
+                result=(RESULT_COLUMN_ERROR, None)
+                while result[0] in [RESULT_COLUMN_ERROR, RESULT_ROW_ERROR, RESULT_DUPLICATED]:
+                    coordinates = player_battle_zone.random_coordinates()
+                    result=player_battle_zone.fire_shot(coordinates=coordinates, battle_zone=computer_battle_zone)
+                break   # end the Player's turn
+            else:
+                result=player_battle_zone.fire_shot(coordinates=coordinates, battle_zone=computer_battle_zone)
+                if result[0] in [RESULT_COLUMN_ERROR, RESULT_ROW_ERROR]:
+                    print("Sorry, the coordinates are incorrect, Please try again ")
+                    delay(2)
+                elif result[0]==RESULT_DUPLICATED:
+                    print("Sorry, the coordinates are duplicated, Please try again ")
+                    delay(2)
+                else:
+                    break
+        # Draw screen with updated information
+        display_battle_zone(player_battle_zone) # Display battle zone
+        if sum([ship.sunk for ship in computer_battle_zone.ships]) == len(computer_battle_zone.ships):
+            # All the Computer's  ships are sunk. End of the game
+            winner = player_battle_zone
+            break    # end the game # The player wins
+
+        print(f"Result={result}")
+        input("The computer will firing now, Press [return] to continue")
+        #delay(1)
+        while True: # compuer's turn loop
+            coordinates = computer_battle_zone.random_coordinates()
+            result=computer_battle_zone.fire_shot(coordinates=coordinates, battle_zone=player_battle_zone )
+            if result[0] not in [RESULT_COLUMN_ERROR, RESULT_ROW_ERROR, RESULT_DUPLICATED]:
+                break # end the Computer's turn
+
+    return winner
+
+
 
 def get_grid_size(name, cols, rows):
     '''Requests the user the size of the board in columns and rows.
