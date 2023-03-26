1c53414a4c61b5381b65fe481505d884ae96c88f
Juan Manuel de las Heras Arroyo <j.heras13@gmail.com>
2023-03-26 06:35:55 +0000

    Add file battleships_classes.py

---
+++ battleship_classes.py
@@ -0,0 +1,12 @@
+'''
+File: battleships_classes.py
+Date: 2023-03-22
+Author: Juan Manuel de las Heras Arroyo
+
+Description:
+This file contains the definition of the ships classes used in the Battleships
+project.
+It also defines all the constats used to describe all the ships and game board
+elements internally and to be displayed at the console.
+'''
+
